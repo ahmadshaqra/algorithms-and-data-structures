b0e39a5d0e7a360f1f14de92da191b86cd8b0c9e
ahmadshaqra <ahmad.shaqra1@gmail.com>
2024-11-02 00:36:45 +1100

    Added utilility module (util.py) and implemented integer validation functions.

---
+++ other/utils.py
@@ -0,0 +1,80 @@
+"""
+    Utils
+
+    A module of utility functions.
+
+    Author: Ahmad Abu-Shaqra
+    Last Modified: 01/11/24
+"""
+
+def validate_int(value: int, *, min_value: int | None = None, max_value: int | None = None) -> bool:
+    """
+        Validates an integer with optional lower and upper bounds.
+
+        Time Complexity:
+            Worst Case: O(1)
+            Average Case: O(1)
+            Best Case: O(1)
+        
+        Space Complexity:
+            Worst Case: O(1)
+            Average Case: O(1)
+            Best Case: O(1)
+
+        Args:
+            value (int): the value to be validated.
+            min_value (int): the lower bound value.
+            max_value (int): the upper bound value.
+        
+        Returns:
+            bool: true if integer is valid, false otherwise.
+        
+        Raises:
+            TypeError: if value, min_value, or max_value are not 'int'.
+    """
+    
+    # validates type of value
+    validate_int_type(value)
+    
+    # validates type of min_value
+    if min_value is not None:
+        validate_int_type(min_value)
+    
+    # validates type of max_value
+    if max_value is not None:
+        validate_int_type(max_value)
+
+    # validates lower bound
+    if min_value is not None and value < min_value:
+        return False
+    
+    # validates upper bound
+    if max_value is not None and value > max_value:
+        return False
+    
+    # integer is validated
+    return True
+
+def validate_int_type(value: int) -> None:
+    """
+        Validates an integer's type.
+
+        Time Complexity:
+            Worst Case: O(1)
+            Average Case: O(1)
+            Best Case: O(1)
+
+        Space Complexity:
+            Worst Case: O(1)
+            Average Case: O(1)
+            Best Case: O(1)
+
+        Args:
+            value (int): the value to be validated.
+
+        Raises:
+            TypeError: if value is not of type 'int'.
+    """
+
+    if not isinstance(value, int) or isinstance(value, bool):
+        raise TypeError(f"expected 'int' but got '{type(value).__name__}' instead.")
